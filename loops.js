54535d5221e86ed953eae4683c1ae6afc664dda3
Arella Gussow <arellaeg@gmail.com>
2018-06-06 14:01:28 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-function forLoop (var i=1, i<25, i++, var array=[]){
+function forLoop (var i=1, i<=25, i++, var array=[]){
   if i===1 {array.push(`I am ${i} strange loop.`)}
   else {array.push(`I am ${i} strange loops.`)}
 }
