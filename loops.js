029cbb4d99512821e7d1daa984e4d74a3fc643c3
Arella Gussow <arellaeg@gmail.com>
2018-06-06 14:05:58 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,9 @@
 function forLoop(array){
-  for(et i=0, i=<25, i++, var array=[]) {
-  if i===1 {array.push(`I am ${i} strange loop.`)}
-  else {array.push(`I am ${i} strange loops.`)}
+  for(let i=0, i=<25, i++, var array=[]) {
+  if i===1 {
+    array.push(`I am ${i} strange loop.`)
+  }
+  else {
+    array.push(`I am ${i} strange loops.`)
+  }
 }
